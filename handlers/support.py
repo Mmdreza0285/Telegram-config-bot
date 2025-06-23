5473731bea6a401617ddc462e9989db4aa61df5a
shadoowvpnn <mohammadreza138112@gmail.com>
2025-06-23 05:18:14 +0330

    Update support.py

--- handlers/support.py
+++ handlers/support.py
@@ -6,4 +6,3 @@ router = Router()
 @router.message(lambda m: m.text == "🆘 پشتیبانی")
 async def support_start(message: Message):
     await message.answer("📩 پیام خود را ارسال کنید، تیم پشتیبانی به زودی پاسخ می‌دهد.")
-
